52828274c51d05708e63ff8fe89241a2bd4bd431
Einsbernnn <dendiass18@gmail.com>
2026-04-11 21:17:04 +0800

    feat: update MainLayout with bottom navigation and enable Notify plugin

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,35 +2,72 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
+        <q-toolbar-title>{{ appName }}</q-toolbar-title>
         <q-btn
+          v-if="authStore.isLoggedIn"
           flat
           dense
           round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
+          icon="logout"
+          @click="authStore.logout"
         />
-
-        <q-toolbar-title> {{ appName }} </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
+        <q-btn
+          v-else
+          flat
+          label="Sign In"
+          to="/auth"
+        />
       </q-toolbar>
     </q-header>
 
     <q-page-container>
       <router-view />
     </q-page-container>
+
+    <q-footer elevated>
+      <q-tabs
+        v-model="activeTab"
+        class="bg-primary text-white"
+        active-color="white"
+        indicator-color="white"
+        align="justify"
+        narrow-indicator
+      >
+        <q-route-tab
+          name="home"
+          to="/"
+          icon="home"
+          label="Home"
+        />
+        <q-route-tab
+          name="sessions"
+          to="/sessions"
+          icon="receipt_long"
+          label="Sessions"
+        />
+        <q-route-tab
+          name="add"
+          to="/sessions/new"
+          icon="add_circle"
+          label="New Session"
+        />
+        <q-route-tab
+          name="settings"
+          to="/settings"
+          icon="settings"
+          label="Settings"
+        />
+      </q-tabs>
+    </q-footer>
   </q-layout>
 </template>
 
 <script setup lang="ts">
   import { ref } from 'vue';
   import { APP_NAME } from '../constants/app';
+  import { useAuthStore } from 'src/stores/useAuthStore';
 
   const appName = APP_NAME;
-  const leftDrawerOpen = ref(false);
-
-  function toggleLeftDrawer() {
-    leftDrawerOpen.value = !leftDrawerOpen.value;
-  }
+  const activeTab = ref('home');
+  const authStore = useAuthStore();
 </script>
